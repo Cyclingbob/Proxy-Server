1df344021bd852971aa79afdf148bd981d46396b
Cyclingbob <52578139+Cyclingbob@users.noreply.github.com>
2021-08-05 16:29:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const http = require('http')
 const errors = require('./assets/errors')
 const panel = require('./panel/panel')
+const config = require('./config')
+
+const mongoose = require('mongoose')
+
+mongoose.connect(
+    config.mongodb_connection_string,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+);
 
 function server(){
     http.createServer(require('./assets/request')).listen(80)
