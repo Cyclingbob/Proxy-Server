6cde6e56f8e999fd54866778a466ceb4641fcb16
Cyclingbob <52578139+Cyclingbob@users.noreply.github.com>
2021-08-05 16:34:24 +0100

    Update logs.js

--- assets/logs.js
+++ assets/logs.js
@@ -1,23 +1,13 @@
 const fs = require('fs')
+const logs = require('../models/logs')
+const mongoose = require('mongoose')
 
 const actions = {
     add: function(options){
 
-        fs.readFile(__dirname.substring(__dirname.length - 6, __dirname) + '/data/logs/logs.json', 'utf8', function(err, logs){
+        options._id = mongoose.Types.ObjectId().toString()
 
-            if(err) return console.log(err)
-
-            logs = JSON.parse(logs)
-
-            logs.push(options)
-
-            fs.writeFile(__dirname.substring(__dirname.length - 6, __dirname) + '/data/logs/logs.json', JSON.stringify(logs), (err) => {
-    
-                if(err) return console.log(err);
-    
-            });
-
-        })
+        logs.create(options)
 
     },
 
