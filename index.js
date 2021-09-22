479e29309a52312704946774f3fd1febdb6b4517
Cyclingbob <52578139+Cyclingbob@users.noreply.github.com>
2021-09-22 09:53:45 +0100

    Update index.js
    Plugins are here!

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const http = require('http')
+const { EventEmitter } = require('events')
+
 const errors = require('./assets/errors')
 const panel = require('./panel/panel')
 const config = require('./config')
+
+const getrules = require('./assets/getrules')
+const getzones = require('./assets/getzones')
+const getip = require('./assets/getip')
 
 const mongoose = require('mongoose')
 
@@ -13,10 +19,47 @@ mongoose.connect(
     }
 );
 
-function server(){
-    http.createServer(require('./assets/request')).listen(80)
-}
+(async () => {
 
-server()
-errors()
-panel()
+    var rules = await getrules()
+    var zones = await getzones()
+
+    var cache = { zones, rules }
+
+    var ip = await getip()
+
+    const emitter = new EventEmitter()
+
+    emitter.on('cacheAdd', function(new_item){
+
+        cache.push(new_item)
+
+    })
+
+    function server(ip, cache, emitter){
+
+        http.createServer(function(req, res){
+            emitter.emit('request', req, res, function(boolean){
+                if(boolean === true) return require('./assets/request')(req, res, ip, cache, emitter)
+            })
+        })
+        .listen(80)
+
+    }
+
+    const fs = require('fs')
+    const scriptsFolder = `${__dirname}/plugins/`
+
+    const files = fs.readdirSync(scriptsFolder) // reading files from folders
+    files.map(function(file) {
+        var plugin = require(scriptsFolder + file)
+        plugin(emitter)
+    })
+
+    server(ip, cache, emitter)
+    errors()
+    panel(getip(), cache)
+
+    console.log('ready')
+
+})();
