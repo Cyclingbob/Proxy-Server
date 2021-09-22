4ad78abc59edb0939ba43532dadd9236466df39a
Cyclingbob <52578139+Cyclingbob@users.noreply.github.com>
2021-09-22 09:55:43 +0100

    Update request.js
    Plugins are here!

--- assets/request.js
+++ assets/request.js
@@ -3,48 +3,78 @@ const logs = require('./logs')
 const fetch = require('node-fetch')
 const config = require('../config.js')
 
-async function request(req, res){
+async function request(req, res, server_ip, cache, emitter){
 
     var ip, country
+    server_ip = server_ip.ip
 
-    if(req.headers['cf-connecting-ip']){
+    if(req.headers['cf-connecting-ip']) ip = req.headers['cf-connecting-ip']
+    else ip = req.connection.remoteAddress.split(':')[3]
 
-        ip = req.headers['cf-connecting-ip']
+    if(req.headers['cf-ipcountry'])country = req.headers['cf-ipcountry']
+    else country = 'Unknown'
 
-    } else{
+    if(config.banned_keywords.length > 0){
+        if(config.banned_keywords.some(a => req.url.includes(a))){
+            fetch(`https://api.cloudflare.com/client/v4/accounts/${config.cloudflare_account_id}/firewall/access_rules/rules`, {
+                headers: {
+                    "X-Auth-Key": config.cloudflare_api_key,
+                    "X-Auth-Email": config.cloudflare_email,
+                    "Content-Type": "application/json"
+                },
+                method: "POST",
+                body: JSON.stringify({
+                    mode: "block",
+                    configuration: {
+                        target: "ip",
+                        value: ip
+                    }
+                })
+            })
+            .then(data => data.json())
+            .then(data => {
 
-        ip = req.connection.remoteAddress.split(':')[3]
+                var blockdata = {
+                    type: 'blocked',
+                    domain: req.headers.host,
+                    time: Date.now(),
+                    ip,
+                    path: req.url,
+                    country
+                }
+            
+                logs.add(blockdata)
+                
+                data.result.paused = false
 
+                emitter.emit('cacheAdd', data.result)
+                emitter.emit('blocked', "banned_keywords", blockdata, data)
+
+            })
+
+            return res.sendStatus(403)
+        }
     }
 
-    if(req.headers['cf-ipcountry']){
+    if(cache.rules.some(a => a.configuration.value === ip && a.mode === 'block')) return;
 
-        country = req.headers['cf-ipcountry']
+    if(config.block_ip_access === true){
+        if(req.headers.host.includes(server_ip) || ip.includes(':')){
+            var data = {
+                type: 'ignored',
+                domain: req.headers.host,
+                time: Date.now(),
+                ip,
+                path: req.url,
+                country
+            }
 
-    } else {
-
-        country = 'Unknown'
-
+            emitter.emit('ignored', "direct_ip_access", data, req, res)
+        
+            logs.add(data)
+            return
+        }
     }
-
-    var data = await fetch(`https://api.cloudflare.com/client/v4/accounts/${config.cloudflare_account_id}/firewall/access_rules/rules`, {
-        headers: {
-            "X-Auth-Key": config.cloudflare_api_key,
-            "X-Auth-Email": config.cloudflare_email,
-            "Content-Type": "application/json"
-        },
-        method: "GET",
-    })
-    .then(data => data.json())
-    .then(data => {
-
-        if(data.result.includes(a => a.configuration.value === ip && a.mode === 'blocked')) return { block: true }
-
-        return { block: false }
-
-    })
-
-    if(data.block === true) res.sendStatus(403)
 
     var data = {
         type: 'visit',
@@ -55,9 +85,11 @@ async function request(req, res){
         country
     }
 
+    emitter.emit('visit', data)
+
     logs.add(data)
 
-    proxy(req, res)
+    proxy(req, res, emitter)
 
 }
 
