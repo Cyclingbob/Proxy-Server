cdba6ff256ed133340aab33c9812f27713c9cff9
Cyclingbob <52578139+Cyclingbob@users.noreply.github.com>
2021-09-22 09:54:18 +0100

    Update config.js
    Plugins are here!

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const config = {
     //copy the below in {} for each user
     users: [
         {
-            username: "Admin",
-            password: "Test",
-            email: "test@test.com"
+            username: "Cyclingbob",
+            password: "Enterprise1",
+            email: "bob21567@yahoo.com"
         }
     ],
 
@@ -29,18 +29,25 @@ const config = {
     If the value is "false", time would be formatted as "8:7:4"
     */
 
-    use_0_in_time: true
-    
+    use_0_in_time: true,
+
     /* These items below allows this server to access Cloudflare API. This is useful if you want to block/unblock and manage IPs visiting your site. These are required. */
 
-    cloudflare_api_key: '',
-    cloudflare_email: '',
-    cloudflare_account_id: '',
+    cloudflare_api_key: '418d0b810761e91d9b15baaa98e4c735ca707',
+    cloudflare_email: 'bob21567@yahoo.com',
+    cloudflare_account_id: 'a9fd6f9c500b9a7785f520dd9f2aa538',
 
     /* This allows the server to save requests to mongodb. This is required */
 
-    mongodb_connection_string: ''
+    mongodb_connection_string: 'mongodb+srv://dns:datingsite@cluster0.qqw2s.mongodb.net/dns?retryWrites=true&w=majority',
 
+    /* banned keywords allows your server to automatically ban ips based on keywords in this array. If you don't want it to do this leave the array empty: [] */
+
+    banned_keywords: ['robots.txt', 'php', '.html', '/boaform/admin/formLogin'],
+
+    /* This option below allows you to decide if you want your server to ignore requests to its own IP. This means all requests must be made through domains */
+
+    block_ip_access: true,
     
 }
 
