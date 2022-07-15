c934ecf9a3006b7fff469556a90ba90552cf36f9
Cyclingbob <52578139+Cyclingbob@users.noreply.github.com>
2022-07-15 20:02:06 +0100

    Delete getip.js

--- assets/getip.js
+++
@@ -1,6 +0,0 @@
-const fetch = require('node-fetch');
-
-module.exports = async function(){
-    var ip = await fetch('https://ip.seeip.org/json').then(data => data.json()).then(data => { return data })
-    return ip
-}
